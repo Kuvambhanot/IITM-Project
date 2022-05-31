6fa683f7426b15174192fd90d3441f3af6096cc5
Madhavkumar1234 <106524647+Madhavkumar1234@users.noreply.github.com>
2022-05-31 14:48:09 +0530

    code

---
+++ main.js
@@ -0,0 +1,4 @@
+var a=document.getElementsByName("kuvam");
+var b=document.getElementsByName("madhav");
+var c=a.addEventListener("mouseover",alert("hello"));
+var d=b.addEventListener("click",alert("hello"));
